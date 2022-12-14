e3ec2bc82ec91edd123e621a00624fdfa7513e56
techchrism <chris@techchrism.me>
2022-12-13 18:34:36 -0800

    bump ServerJarLoader version

--- src/main/kotlin/me/techchrism/enchantmentjsonexporter/ServerJarLoader.kt
+++ src/main/kotlin/me/techchrism/enchantmentjsonexporter/ServerJarLoader.kt
@@ -12,37 +12,37 @@ import java.util.jar.JarInputStream
 
 class ServerJarLoader {
     companion object {
-        const val VERSION = "1.2.0"
-        
+        const val VERSION = "1.2.1"
+
         fun loadVersionManifest(): VersionManifestV2 {
             val jsonText = URL("https://piston-meta.mojang.com/mc/game/version_manifest_v2.json").readText()
             val gson = Gson()
             return gson.fromJson(jsonText, VersionManifestV2::class.java)
         }
-        
+
         fun loadVersionJson(url: String): VersionJson {
             val jsonText = URL(url).readText()
             val gson = Gson()
             return gson.fromJson(jsonText, VersionJson::class.java)
         }
-        
+
         fun getEnchantmentsFor(version: String? = null): String? {
             val manifest = loadVersionManifest()
             val targetVersion = version ?: manifest.latest.release
             val foundVersion = manifest.versions.find { it.id == targetVersion } ?: return null
             return getEnchantmentsForVersionJson(loadVersionJson(foundVersion.url))
         }
-        
+
         fun getEnchantmentsForVersionJson(version: VersionJson): String {
             return remapJarForEnchantments(
                 JarInputStream(URL(version.downloads.server.url).openStream()),
                 URL(version.downloads.server_mappings.url).openStream()
             )
         }
-        
+
         fun remapJarForEnchantments(bundledJarStream: JarInputStream, mappings: InputStream): String {
             val mapping = JarMapping()
-            BufferedReader(InputStreamReader(mappings)).use { 
+            BufferedReader(InputStreamReader(mappings)).use {
                 mapping.loadMappings(it, null, null, false)
             }
 
